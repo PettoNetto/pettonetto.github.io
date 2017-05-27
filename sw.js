61c73326bfac49a5b17b62f5f19de05472876c92
Leonardo Y. M <leoyasu@yahoo.com.br>
2017-05-27 09:56:25 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/loja-adidas.html',
+       '/loja-bacio-di-latte.html',
+       '/loja-brooksfield.html',
+       '/loja-burberry.html'
+
+     ]);
+   })
+ );
+});
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
